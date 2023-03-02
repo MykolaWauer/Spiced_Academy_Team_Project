943386134bf997cc5db7c85e6ae24befdaa7e2de
Chris Williams <20cwilliams09@gmail.com>
2023-03-02 14:53:34 +0100

    Added in CLI capabilities.

--- simulation/sim_day.py
+++ simulation/sim_day.py
@@ -7,11 +7,11 @@ class Supermarket:
     """
 
     def __init__(self, no_customers) -> None:
-        self.no_customers = no_customers
+        self.no_customers = no_customers+1
 
     def sim_day(self):
 
-        cust_list = range(1, self.no_customers+1)
+        cust_list = range(1, self.no_customers)
 
         df_full = pd.DataFrame()
 
@@ -25,3 +25,16 @@ class Supermarket:
         df_full.set_index('time', inplace=True)
 
         return df_full
+    
+if __name__ == "__main__":
+    print("Let's simulate a day in your store! \n")
+    store_name = input("What is the name of your store? ")
+    cus_no = int(input("How many customers will visit your store? "))
+    store = Supermarket(cus_no)
+    print(f"\nToday at {store_name}, {cus_no} people visited the store and went around to these places.")
+    
+    with pd.option_context('display.max_rows', None,
+                       'display.max_columns', None,
+                       'display.precision', 3,
+                       ):
+        print(store.sim_day())
