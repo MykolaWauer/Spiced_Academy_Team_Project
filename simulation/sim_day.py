fe17ea03184fd9aa0e7f1c7c8811dc884584d46f
Chris Williams <20cwilliams09@gmail.com>
2023-03-03 16:08:17 +0100

    cleaned with pylint. and changed to track checkout.

--- simulation/sim_day.py
+++ simulation/sim_day.py
@@ -1,3 +1,9 @@
+"""Creates a DataFrame and a CSV file which tracks customer behavior
+inside a fake grocery store, using probabilities which can be changed.
+
+Returns:
+    _DataFrame_: A DataFrame with 'time','id', and 'location' columns.
+"""
 import pandas as pd
 from customer import Customer
 
@@ -10,6 +16,11 @@ class Supermarket:
         self.no_customers = no_customers+1
 
     def sim_day(self):
+        """Simulates a full day of customer behavior.
+
+        Returns:
+            _DataFrame_: Contains 'time', 'id', and 'location'
+        """
 
         cust_list = range(1, self.no_customers)
 
@@ -20,34 +31,37 @@ class Supermarket:
             customer_obj = Customer(customer_name)
             temp_df = customer_obj.sim_day()
             df_full = pd.concat([df_full, temp_df])
-        
-        fill_time = pd.to_datetime('08:00')
-        df_full.loc[-1] = [fill_time,'drinks',0.2]
+
         df_full.sort_values('time', inplace=True)
         df_full.set_index('time', inplace=True)
-        
+
         return df_full
-    
+
     def make_csv(self, df_full):
-        
+        """Creates a CSV file
+
+        Args:
+            df_full (_DataFrame_): A DataFrame to save.
+        """
+
         df_full.to_csv(f'../data/{store_name}.csv')
 
-    
+
 if __name__ == "__main__":
     print("\nLet's simulate a day in your store! \n")
+
     store_name = input("What is the name of your store?\n")
     cus_no = int(input("How many customers will visit your store?\n"))
     printing = input("Would you like to print? [y/n]\n")
 
     store = Supermarket(cus_no)
-    
-    
+
     df = store.sim_day()
     store.make_csv(df)
 
-    if printing == 'y' or printing == 'yes':
-        
-        print(f"\nToday at {store_name}, {cus_no} people visited the store and went around to these places.")
+    if printing in ('y', 'yes'):
+
+        print(f"\nToday at {store_name}, {cus_no} people visited these places:")
         with pd.option_context('display.max_rows', None,
                         'display.max_columns', None,
                         'display.precision', 3,
